96a7c2379e8880a67d656a339f163648516ce144
Tristan Ackermann <tristan@al.co.za>
2022-08-17 08:51:55 +0200

    Drafted header and dummy available meals list

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,19 @@
 import React from "react";
+import HeaderCartButton from "./HeaderCartButton";
 import classes from "./Header.module.css";
 import imgMeals from "../../assets/meals.jpg";
 
 function Header(props) {
-    return (
-        <React.Fragment>
-            <header className={classes.header}>
-                <h1>ReactMeals</h1>
-                <button>Cart</button>
-            </header>
-            <div className={classes["main-image"]}>
-                <img src={imgMeals} alt="A table full of delicious food!" />
-            </div>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={imgMeals} alt="A table full of delicious food!" />
+      </div>
+    </React.Fragment>
+  );
 }
 export default Header;
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,12 +1,43 @@
 import React from "react";
+import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 
-function AvailableMeals(props) {
-    return (
-        <React.Fragment>
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 
-        </React.Fragment>
-    );
-};
+function AvailableMeals(props) {
+  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+}
 
 export default AvailableMeals;
